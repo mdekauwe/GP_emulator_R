6e0d9040058d719f4f40f332efb1c1c62208a523
mdekauwe <mdekauwe@gmail.com>
2017-02-19 09:01:22 +1100

    Tweaked script

--- test_gday_emulator.R
+++ test_gday_emulator.R
@@ -3,38 +3,25 @@
 library(DiceKriging)
 library(ggplot2)
 
-
+# Train on the first 10 years
+yr = 2005
 df_met <- read.csv(file="DUKE_met_data_amb_co2.csv", header=TRUE, sep=",")
-
-# Drop the last year, i.e. train on first part, predict the last bit
-x1 <- subset(df_met, year < 2007)
-x2 <- subset(df_met, year == 2007)
-
-keeps <- c("tair", "rain","vpd_am","vpd_pm","co2","ndep","wind","par_am","par_pm")
-x1 <- x1[keeps]
-x3 <- x2["doy"]
-x2 <- x2[keeps]
-
-
+x1 <- subset(df_met, year<=yr)
+keep <- c("tair", "rain","vpd_am","vpd_pm","par_am","par_pm")
+x1 <- x1[keep]
 x1 <- data.matrix(x1)
 
-
+# Get the model response
 df_mod <- read.csv(file="D1GDAYDUKEAMB.csv", header=TRUE, sep=",")
-
-# Drop the last year, i.e. train on first part, predict the last bit
-y1 <- subset(df_mod, YEAR < 2007)$GPP
-y2 <- subset(df_mod, YEAR == 2007)$GPP
+y1 <- subset(df_mod, YEAR <= yr)$GPP
 
 # Fit the emaulator
 gp_model <- km(~., design=data.frame(x=x1), response=y1)
 
-# Use the emaulator to make some predictions
-t <- seq(0, 365, by=1)
+# Use the emulator to make some predictions for the whole timeseries
+x2 <- df_met[keep]
+x2 <- data.matrix(x2)
 p <- predict(gp_model, newdata=data.frame(x=x2), type="SK")
 
-plot(x3$doy, p$mean, type = "l", xlim = c(0, 365), ylim = c(0, 10),
-     xlab = "x", ylab = "y")
-lines(x3$doy, y2, col = "green")
-#lines(x3$doy, p$lower95, col = "black", lty = 2)
-#lines(x3$doy, p$upper95, col = "black", lty = 2)
-points(x3$doy, y2, col = "red", pch = 19)
+plot(p$mean, type="l", ylim=c(0, 15), xlab="DOY", ylab="GPP")
+lines(df_mod$GPP, col = "green")
